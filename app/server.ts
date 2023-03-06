198bd038e3b3da8886dee8485c91f295f90454c1
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2023-03-06 17:18:07 -0300

    add dotenv

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,9 @@
 import Fastify from 'fastify'
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 const fastify = Fastify()
 
-fastify.listen({ port: 3333 }, () => {
-  console.log('Server on')
+fastify.listen({ port: Number(process.env.PORT!) }, () => {
+  console.log('Server on port ' +  Number(process.env.PORT!))
 })
